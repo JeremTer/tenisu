e43934ecd758e68ba34e75a61fb65a073326576e
JeremTer <ternisien09@gmail.com>
2023-05-11 12:59:27 +0200

    Project created

---
+++ src/main/java/com/abbeal/App.java
@@ -0,0 +1,13 @@
+package com.abbeal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
